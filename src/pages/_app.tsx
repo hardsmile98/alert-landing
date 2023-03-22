c4ba98f2aadd5c2936d81164a2f415a896be590e
Денис Котелев <deniskotelev@MacBook-Pro-Denis.local>
2023-03-22 20:40:18 +0300

    offer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,26 @@
 import '../styles/normalize.css';
 import '../styles/global.css';
+import { YMInitializer } from 'react-yandex-metrika';
 import type { AppProps } from 'next/app';
 
+const YANDEX_ID = Number(process.env.NEXT_PUBLIC_YANDEX_ID);
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {YANDEX_ID && (
+        <YMInitializer
+          accounts={[YANDEX_ID]}
+          options={{
+            webvisor: true,
+            defer: true,
+          }}
+          version="2"
+        />
+      )}
+
+      <Component {...pageProps} />
+      ;
+    </>
+  );
 }
